f18c1917ded22c016d44e6c335ae0bd58a4a8b68
tomatoos404 <thomas.touny991@gmail.com>
2026-02-11 10:21:45 +0100

    ajout gestion de erreur

--- application.js
+++ application.js
@@ -10,3 +10,9 @@ app.get('/message', (req, res) => {
 app.listen(port, () => {
   console.log(`Serveur lancé sur http://${monip}:${port}`);
 });
+
+// Gestionnaire d'erreurs global
+process.on('uncaughtException', (err) => {
+  console.error(`Une erreur non capturée s'est produite : ${err.message}`);
+  process.exit(1); // 1 signifie une sortie avec une erreur
+});
