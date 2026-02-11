1e8aa4b3daddba86506b29b0ffc7993c0ed016ed
tomatoos404 <thomas.touny991@gmail.com>
2026-02-11 11:10:28 +0100

    ajout de la route post

--- application.js
+++ application.js
@@ -2,14 +2,30 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const monip = '172.29.18.111' ;
+const os = require('os');
+const bodyParser = require('body-parser');
 
-app.get('/message', (req, res) => {
+app.get('/message_get', (req, res) => {
   res.send('Bonjour mes poulets');
 });
 
-app.listen(port, () => {
-  console.log(`Serveur lancé sur http://${monip}:${port}`);
+
+app.post('/message_post', (req, res) => {
+  const donneesDuCorps = req.body;
+  console.log(donneesDuCorps);
+  res.send('Données reçues et traitées !');
 });
+
+
+// (req, res) => {res.send('Bonjour mes poulets')};
+
+app.listen(port, () => {
+  console.log(`le serveur est sur ecoute ${os.networkInterfaces()['ens18'][0].address}:${port}`);
+});
+
+//middleware 
+app.use(bodyParser.json()); // décode le body d'une requête
+
 
 // Gestionnaire d'erreurs global
 process.on('uncaughtException', (err) => {
